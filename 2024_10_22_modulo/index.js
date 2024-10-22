5d7fc524e35f9132cf33e6574415e93d80011e39
Uriel Haile <perenganito@gmail.com>
2024-10-22 15:33:10 -0600

    Se modifican dos lienzos a la vez

--- 2024_10_22_modulo/index.js
+++ 2024_10_22_modulo/index.js
@@ -1,13 +1,30 @@
-import { select, scaleOrdinal, pie, arc } from 'd3';
+import { select, arc, pie, scaleOrdinal } from 'd3';
 
-let svg = select('#pie')
+let svg1 = select('#grafica1')
   .append('svg')
   .attr('width', 200)
   .attr('height', 200)
   .append("g")
   .attr("transform", "translate(100,100)");
 
-//const data = {a: 9, b: 20, c:30, d:8, e:12};
+let svg2 = select('#grafica2')
+  .append('svg')
+  .attr('width', 200)
+  .attr('height', 200)
+  .append("g")
+  .attr("transform", "translate(100,100)");
+ 
+const drawRect = (w, h, x, y, color, svg) => {
+  const rect = svg.append("rect");
+  rect.attr("width", w);
+  rect.attr("height", h);
+  rect.attr("x", x);
+  rect.attr("y", y);
+  rect.attr("rx", 4);
+  rect.attr("ry", 4);
+  rect.attr("fill", color);
+};
+
 const mujeres = {"Morena": 100, 
                 "PAN": 56, 
                 "PRI":36, 
@@ -17,17 +34,6 @@ const mujeres = {"Morena": 100,
                 "PRD": 7, 
                 "SP":2};
 const data = mujeres;                
-/*
-MORENA	100	50	100	50	200	40
-PAN	    56	50	56	50	112	22.4
-PRI	36	52.9	32	47.1	68	13.6
-PVEM	19	47.5	21	52.5	40	8
-PT	16	48.5	17	51.5	33	6.6
-MC	15	51.7	14	48.3	29	5.8
-PRD	7	58.3	5	41.7	12	2.4
-SP	2	33.3	4	66.7	6
-*/
-
 const color = scaleOrdinal()
   .range(["#A72A2FFF", "#01308F", "#FF0000", "#50B948", "#FA1E28","#FF881A", "#FFCC00", "#606060FF"]);
 // Compute the position of each group on the pie:
@@ -42,12 +48,13 @@ data_ready.forEach((data,index)=>{
     .startAngle(data.startAngle)
     .endAngle(data.endAngle);
 
-  svg.append('path')
+  svg1.append('path')
     .attr('d',arcObj)
     .attr("fill", color(index))
     .attr("stroke", "black")
     .attr("stroke-width", 1);
 });
 
-/*
-*/
+
+//drawRect(100, 200, -100, -200, "#F90000FF", svg1);
+drawRect(100, 200, 0, 0, "#D8F900FF", svg2);
