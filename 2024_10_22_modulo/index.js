8f57e00b16c3b3404059a2d0b2b6f1ab83af1cf0
Uriel Haile <perenganito@gmail.com>
2024-10-25 14:52:07 -0600

    Se mueve el código de la gráfica de barras

--- 2024_10_22_modulo/index.js
+++ 2024_10_22_modulo/index.js
@@ -6,4 +6,13 @@ const svg = select("#draw")
             .attr("width", 200)
             .attr("height", 200);
 
+const data = {
+  "León" : 85,
+  Irapuato: 52,
+  Celaya: 27,
+  Salamanca: 21,
+  "Valle de Santiago": 18
+};
+
+
 drawRect(100,50, 100,10,"#E32222FF",svg);
--- 2024_10_22_modulo/utils/draw.js
+++ 2024_10_22_modulo/utils/draw.js
@@ -1,3 +1,5 @@
+import { scaleBand, axisBottom, scaleLinear, axisLeft } from "d3";
+
 export const drawRect = (w, h, x, y, color, svg) => {
   const rect = svg.append("rect");
   rect.attr("width", w);
@@ -8,3 +10,38 @@ export const drawRect = (w, h, x, y, color, svg) => {
   rect.attr("ry", 4);
   rect.attr("fill", color);
 };
+
+export const drawImage = (w, h, x, y, link, svg) => {
+  const img = svg.append('image');
+  img.attr('x',x);
+  img.attr('y',y);
+  img.attr('width',w);
+  img.attr('height',h);
+  img.attr('href', link);
+}
+
+
+export const drawBarchart = () => {
+
+let x_axis = scaleBand(keys, [0, 100]);
+const g_x_axis = svg.append('g')
+  .attr('transform', "translate("+ 50 +","+100+")")
+  .call(axisBottom(x_axis));
+
+g_x_axis.selectAll("text")
+  .attr("transform", "translate(-10,0)rotate(-40)")
+  .style("text-anchor", "end");
+
+let y_axis = scaleLinear([0, 100],[100, 0]);
+const g_y_axis = svg.append('g')
+  .attr('transform', 'translate(50, 0)')
+  .call(axisLeft(y_axis));
+
+
+let values = Object.values(data);
+Object.keys(data).forEach( (ciudad,index) => {
+  console.log(x_axis(ciudad));
+  drawRect(10, values[index],55 + x_axis(ciudad), 100-values[index], '#CD4444FF', svg);  
+  drawImage(10, 10, 55 + x_axis(ciudad), 90-values[index]);
+});
+}
